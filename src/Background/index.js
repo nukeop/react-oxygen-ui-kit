371f5ecddbe5cf13d29701d78e22064e5b658d3b
nukeop <alsw@protonmail.com>
2018-03-31 21:20:24 +0200

    Add proptypes and options to backgrounds

--- src/Background/index.js
+++ src/Background/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 
@@ -11,21 +12,40 @@ class Background extends React.Component {
     let {
       image,
       color1,
-      color2
+      color2,
+      centered,
+      noGradient,
+      colored
     } = this.props;
 
-    let overlayStyle = color1 && color2
-    ? {background: `linear-gradient(135deg, ${color1} 10%, ${color2} 100%)`}
+    let bgImageStyle = {};
+    bgImageStyle.backgroundImage = `url(${image})`;
+    centered ? bgImageStyle.backgroundPosition = 'center center' : null;
+    colored? bgImageStyle.filter = 'grayscale(0)' : null;
+
+    let overlayStyle = {};
+    color1 && color2
+    ? overlayStyle.background = `linear-gradient(135deg, ${color1} 10%, ${color2} 100%)`
     : null;
+    noGradient ? overlayStyle.background = 'transparent' : null;
 
     return (
       <div className='oxygen background'>
-        <div className='bg-image' style={{backgroundImage: `url(${image})`}}></div>
+        <div className='bg-image' style={bgImageStyle}></div>
         <div className='bg-overlay' style={overlayStyle}></div>
         {this.props.children}
       </div>
     );
   }
 }
+
+Background.propTypes = {
+  image: PropTypes.string,
+  color1: PropTypes.string,
+  color2: PropTypes.string,
+  centered: PropTypes.bool,
+  noGradient: PropTypes.bool,
+  colored: PropTypes.bool
+};
 
 export default Background;
