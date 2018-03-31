982b9e673f3e43ba9ce59c3e017711d3b9e4fb5e
nukeop <alsw@protonmail.com>
2018-03-31 02:48:12 +0200

    Extra styles for panels

--- src/Panel/index.js
+++ src/Panel/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import styles from './styles.scss';
 
 const Panel = props => {
   return (
-    <div style={props.style} className="oxygen panel">
+    <div
+      style={props.style}
+      className={classnames("oxygen panel", {"fluid": props.fluid})}
+    >
       {props.children}
     </div>
   );
