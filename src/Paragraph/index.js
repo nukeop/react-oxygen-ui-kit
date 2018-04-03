174cd84582a1a1834dd5ff8dff149fb38d9b7dd3
nukeop <alsw@protonmail.com>
2018-04-03 23:38:01 +0200

    Panel demos

--- src/Paragraph/index.js
+++ src/Paragraph/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import styles from './styles.scss';
@@ -6,11 +7,25 @@ import styles from './styles.scss';
 const Paragraph = props => {
   return (
     <div
-      className={classnames("oxygen paragraph", {"inverted": props.inverted})}
+      className={
+        classnames(
+          "oxygen paragraph",
+          {"inverted": props.inverted},
+          {"fluid": props.fluid}
+        )
+      }
+      style={props.style}
     >
       {props.children}
     </div>
   );
 }
+
+Paragraph.propTypes = {
+  inverted: PropTypes.bool,
+  fluid: PropTypes.bool,
+  className: PropTypes.string,
+  style: PropTypes.object
+};
 
 export default Paragraph;
