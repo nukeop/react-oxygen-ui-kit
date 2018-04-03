53f096526d0b68b6fd5710e2e7c6566ccf4475fd
nukeop <alsw@protonmail.com>
2018-04-03 22:43:53 +0200

    Add attachable elements

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import styles from './styles.scss';
 
 const Input = props => {
   return (
-    <input className="oxygen input" type="text"/>
+    <input
+      className={
+        classnames(
+          "oxygen input",
+          {"border": props.border},
+          {'attachLeft': props.attach === 'left'},
+          {'attachRight': props.attach === 'right'},
+          {'attachMid': props.attach === 'mid'}
+        )
+      }
+      type="text"
+    />
   );
 }
+
+Input.propTypes = {
+  border: PropTypes.bool,
+  attach: PropTypes.string
+};
 
 export default Input;
--- src/PanelBody/index.js
+++ src/PanelBody/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import styles from './styles.scss';
 
 const PanelBody = props => {
   return (
-    <div className="oxygen panel-body">
+    <div
+      className={classnames("oxygen panel-body", {"row": props.row})}
+    >
       {props.children}
     </div>
   );
 }
+
+PanelBody.propTypes = {
+  row: PropTypes.bool
+};
 
 export default PanelBody;
