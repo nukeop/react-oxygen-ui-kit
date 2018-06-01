8c074191ef2c72cfefdcc5c6b5a9511cc00cbc83
nukeop <alsw@protonmail.com>
2018-06-01 21:10:43 +0200

    Update webpack configs to fix build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,7 +10,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?/,
         loader: 'babel-loader'
@@ -24,7 +24,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
     new webpack.NamedModulesPlugin()
   ]
 };
